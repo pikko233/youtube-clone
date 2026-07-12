e8da78ed8873221a1648eb5f58bd6ea270c70ef2
pikko233 <lzs32486@gmail.com>
2026-07-12 17:10:14 +0800

    fix: 优化首页和工作台的导航栏媒体查询

--- src/modules/home/ui/components/home-navbar/index.tsx
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -11,7 +11,7 @@ export const HomeNavbar = () => {
         {/* 菜单和Logo */}
         <div className="flex items-center shrink-0">
           <SidebarTrigger />
-          <Link href="/">
+          <Link href="/" className="hidden md:block">
             <div className="p-2 flex items-center gap-1">
               <Image src="/logo.svg" height={32} width={32} alt="Logo" />
               <p className="text-xl font-semibold tracking-tight">NewTube</p>
